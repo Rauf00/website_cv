ce973f817eeaab8e4256cb2fc4e71b6fbf1e5192
Rauf Shimarov <shimarov00@gmail.com>
2020-10-13 15:51:15 -0700

    Updates

---
+++ precache-manifest.667a8699e93ec9c3ca544eb9e45433ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89426bfe894d312add4881e8ef3949b",
+    "url": "/website_cv/index.html"
+  },
+  {
+    "revision": "a831a9d41a59a1322b83",
+    "url": "/website_cv/static/css/main.c499fefa.chunk.css"
+  },
+  {
+    "revision": "0ad2d030089e9409b8d8",
+    "url": "/website_cv/static/js/2.2e605047.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website_cv/static/js/2.2e605047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a831a9d41a59a1322b83",
+    "url": "/website_cv/static/js/main.e98b4bba.chunk.js"
+  },
+  {
+    "revision": "88c63ddfbdcd9fa9d987",
+    "url": "/website_cv/static/js/runtime-main.c24b223d.js"
+  },
+  {
+    "revision": "957537e2fe55bc2a3f1a557f0fcb3abf",
+    "url": "/website_cv/static/media/about.957537e2.png"
+  },
+  {
+    "revision": "6b0699393d8dfdbc19ca648f5dc6d02e",
+    "url": "/website_cv/static/media/logo.6b069939.png"
+  }
+]);
